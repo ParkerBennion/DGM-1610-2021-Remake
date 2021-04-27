f2986854427dc7cd8d006d2969f09097ea94dac7
Parker <parkerbennion@gmail.com>
2021-04-26 21:48:17 -0600

    not sure 2

---
+++ backup kraken/Proj 2/Assets/Scripts/Projectilemove.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectilemove : MonoBehaviour
+{
+    public int speed;
+  
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        
+    }
+}
---
+++ backup kraken/Proj 2/Assets/Scripts/SpawnManage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnManage : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawnX = 19, spawnZ = 30, spawnInterval = 1.3f, spawnTime = 2;
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(SpawnRate), spawnTime,spawnInterval);
+    }
+    
+    void SpawnRate()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnX, spawnX), 0, spawnZ);
+            
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+}
